2d8bc79d18f8643539000b1d706d029c3a8b4d41
Nicolas-Eng <36778155+Nicolas-Eng@users.noreply.github.com>
2021-05-04 15:24:25 -0700

    Add files via upload

---
+++ AD_GWAS/ATAC_analysis/run_WTC11.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -l h_rt=24:0:0
+#$ -l mem_free=15G
+#$ -S /bin/bash
+#$ -cwd
+#$ -j y
+
+
+cd /shen/shenlabstore3/neng/20200309_MGEN/ATAC_analysis
+conda activate encode-atac-seq-pipeline
+ 
+caper run /shen/shenlabstore3/neng/dependencies/atac-seq-pipeline-master/atac.wdl -i /shen/shenlabstore3/neng/20200309_MGEN/ATAC_analysis/WTC11.json --cromwell /wynton/home/shen/neng/.caper/cromwell_jar/cromwell-47.jar --womtool /wynton/home/shen/neng/.caper/womtool_jar/womtool-47.jar
