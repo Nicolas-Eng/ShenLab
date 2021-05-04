05fea527055ce8a6cd7f80ce274eb55de186858b
Nicolas-Eng <36778155+Nicolas-Eng@users.noreply.github.com>
2021-05-04 15:28:27 -0700

    Add files via upload

---
+++ AD_GWAS/pchic_analysis/run_pchic_ne_exploratory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -l h_rt=336:0:0
+#$ -l mem_free=100G
+#$ -S /bin/bash
+#$ -cwd
+#$ -j y
+
+
+cd /shen/shenlabstore3/neng/20200309_MGEN/scripts
+conda activate pchic-brain
+
+#Rscript pchic_ne_exploratory.R
+Rscript pchic_ne_exploratory_nolow.R
