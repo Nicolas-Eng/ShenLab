27ba6e18c9f48a8a6ff80acd3fb9a9b7cea3d569
Nicolas-Eng <36778155+Nicolas-Eng@users.noreply.github.com>
2021-05-04 16:03:27 -0700

    Delete SNPs_pcHiC_analysis directory

--- SNPs_pcHiC_analysis/SNPs/20200515/expand_SNPs_rmap.sh
+++
@@ -1,9 +0,0 @@
-cat ADSNP_imputeTOPMADexpand_r2_0.8_hg19.bed hg19_17250AD.bed > AD_full_list.bed
-sort -k1,1 -k2,2n AD_full_list.bed > AD_full_list_srt.bed
-#mergeBed -i AD_full_list_srt.bed -c 4,4 -o collapse, count > AD_full_list_srt.merged.bed
-mergeBed -i AD_full_list_srt.bed -c 4 -o collapse > AD_full_list_srt.merged.bed
-bedtools intersect -a AD_full_list_srt.merged.bed -b hg19.rmap -wo > AD_full_SNPs_x_rmap.bed
-cut -f5-7 AD_full_SNPs_x_rmap.bed | bedtools merge -i - -c 1 -o count > AD_full_SNPs_x_rmap_coords_count.bed
-
-bedtools intersect -a AD_full_SNPs_x_rmap_coords_count.bed -b ../distalATAC/MG.optimal.distal.atac.bed > AD_SNPs_rmap_coords_count_x_MG_distal.atac.bed
-bedtools intersect -a AD_full_SNPs_x_rmap_coords_count.bed -b ../distalATAC/EN.optimal.distal.atac.bed > AD_SNPs_rmap_coords_count_x_EN_distal.atac.bed
--- SNPs_pcHiC_analysis/distalATAC/SNPs_x_ATAC.sh
+++
@@ -1,3 +0,0 @@
-bedtools intersect -a AD_full_list_srt.merged.bed -b EN.optimal.distal.res.atac.bed | sort -k1,1 -k2,2n - | bedtools merge -i - | wc -l
-
-bedtools intersect -a AD_full_list_srt.merged.bed -b MG.optimal.distal.res.atac.bed | sort -k1,1 -k2,2n - | bedtools merge -i - | wc -l
